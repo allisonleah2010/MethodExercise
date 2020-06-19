b13e6999222dc27037ce0dcba368310b61a8c576
Allison Cole <allisoncole@allisons-mbp-2.192.168.4.221>
2020-06-19 12:35:26 -0400

    MethodExercise

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! What is your favorite band?");
+            var band = Console.ReadLine();
+        }
+    }
+}
