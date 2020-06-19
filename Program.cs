2f7fa24f39f31a322768d30ed4d8d288cc673433
Allison Cole <allisoncole@allisons-mbp-2.192.168.4.221>
2020-06-19 12:47:47 -0400

    new favorites

--- Program.cs
+++ Program.cs
@@ -17,6 +17,16 @@ namespace MethodExercise
 
             Console.WriteLine("Great! What is your favorite band?");
             var band = Console.ReadLine();
+
+            Console.WriteLine($"Thanks {userName}! Here is your profile");
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
         }
+
     }
 }
+
